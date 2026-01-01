990a9cd865be51267e55b837a2073575e4c8d085
Juan David Lasso <juandavidlasso96@gmail.com>
2026-01-01 12:43:42 -0500

    connection with postgres db

---
+++ server-dvp/src/config/database.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+import { DB_HOST, DB_NAME, DB_PASS, DB_USER } from "./env";
+
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    dialect: "postgres",
+    logging: false,
+});
--- server-dvp/src/server.ts
+++ server-dvp/src/server.ts
@@ -1,10 +1,12 @@
 import { createApp } from "./app";
+import { connectDB } from "./database";
 
 (async () => {
+  await connectDB();
   const app = await createApp();
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}/graphql`);
+    console.log(`Server running on http://localhost:${PORT}/graphql`);
   });
 })();
